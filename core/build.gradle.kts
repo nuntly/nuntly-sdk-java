4bc678e0d0b1c3abf4690ed3d18e5ee80a837cae
Olivier Bazoud <olivier.bazoud@gmail.com>
2026-05-17 15:57:20 +0200

    release: v1.0.0-alpha.16 (#23)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -17,7 +17,7 @@ spotless {
 }
 
 group = "com.nuntly"
-version = "1.0.0-alpha.15"
+version = "1.0.0-alpha.16"
 
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
